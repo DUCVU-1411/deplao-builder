bec3dd17cfec48746146fb50d3ae34fa743b9613
Tiennt <trungtien9x@gmail.com>
2026-05-19 16:21:34 +0700

    feat: initial deploy setup for GitHub Pages

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/deplao-builder/',
+  build: {
+    outDir: 'dist',
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ['three'],
+        },
+      },
+    },
+  },
+});
+
